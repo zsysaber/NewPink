209bb896cd644715440d8d2519e6988ae4255608
zsysaber <zsysaber@yahoo.co.jp>
2021-05-10 09:47:01 +0900

    test1

---
+++ NewPink/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewPink
+//
+//  Created by cho Sigyo on 2021/05/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
